6204d9f31a3c9236ef8093210435b63c4efcbf01
lucianocarvalho <luciano_722@hotmail.com>
2019-05-15 00:04:11 -0300

    Bootstrap and template

--- application/views/dashboard-view.php
+++ application/views/dashboard-view.php
@@ -1,3 +1,11 @@
-<div>
-div
+<div class="row">
+	<div class="col-lg-6">
+		<h4>Bem-vindo ao curso de lógica de programação!</h4>
+		<p>Sinta-se à vontade para estudar como quiser e fazer quantos exercícios quiser!</p>
+		<p>Ah! Não se esqueça de anotar todas as suas dúvidas para tirá-las com um professor, com um colega, ou até mesmo fazer uma pesquisa em livros ou na internet.</p>
+		<p>Bons estudos!</p>
+	</div>
+	<div class="col-lg-6">
+
+	</div>
 </div>
--- application/views/templates/template.php
+++ application/views/templates/template.php
@@ -4,13 +4,39 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap-4.0.0/css/bootstrap.min.css'); ?>">
         <title>SEAALP</title>
     </head>
     <body>
-        <header>Topo</header>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <h1>Meu aprendizado</h1>
+                        <p>Lógica de programação</p>
+                    </div>
 
-        <?php echo $contents; ?>
+                    <div class="col-lg-5">
+                        <a href="<?php echo base_url('login'); ?>" class="btn btn-sm btn-primary">Login</a>
+                        <a href="<?php echo base_url('cadastrar'); ?>" class="btn btn-sm btn-secondary">Cadastre-se</a>
+                        <a href="<?php echo base_url('visitante'); ?>" class="btn btn-sm btn-info">Iniciar como visitante</a>
+                    </div>
+                </div><!-- row -->
+            </div>
+        </header>
 
-        <footer>Rodapé</footer>
+        <div class="container">
+            <?php echo $contents; ?>
+        </div>
+
+        <footer class="text-muted">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        Desenvolvido por alunos da Fatec Ourinhos como trabalho de conclusão de curso.
+                    </div>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
